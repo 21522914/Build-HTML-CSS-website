ac75e552388fe4b29d8e807bfc9f33b1165d0a54
ThuThuy <hathuthuy9999@gmail.com>
2025-03-08 22:58:28 +0700

    bổ sung chức năng video demo

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
-function toggleMenu(){
-    const menu = document.querySelector('.menu-links');
-    const icon = document.querySelector('.hamburger-icon');
-    menu.classList.toggle('open');
-    icon.classList.toggle('open');
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
+
+//play video
+const modalOverlayReactJS = document.getElementById("modalOverlayReactJS");
+const btnPlayDemoReactJS = document.getElementById("btn-play-demo-reactJS");
+const closeModalBtn = document.getElementById('closeModalReactJS');
+const videoReactJS = document.getElementById("videoReactJS");
+
+//Mở Modal bằng nút Live Demo
+btnPlayDemoReactJS.addEventListener("click", () => {
+  modalOverlayReactJS.style.display = "flex";
+  videoReactJS.play(); //tự động phát video
+});
+
+//Đóng Modal
+closeModalBtn.addEventListener("click", () => {
+  modalOverlayReactJS.style.display = "none";
+  videoReactJS.pause(); //tự động tắt video
+});
+
+//Đóng Modal khi click ra ngoài
+window.addEventListener("click", (e) => {
+  if (e.target === modalOverlayReactJS) {
+    modalOverlayReactJS.style.display = "none";
+    videoReactJS.pause(); //tự động tắt video
+  }
+});
